d5d75a7e69a721be2c63d32296dfd0e8a2165414
Swapnil Pawar <swapnildpawar011@gmail.com>
2026-01-19 20:59:26 +0530

    make some minor changes in remove comments

--- src/pages/Homepage/Herosection.jsx
+++ src/pages/Homepage/Herosection.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import '../Homepage/Herosection.css';
 
-// 1. IMPORT BOTH IMAGES
-// Rename your mobile image to 'HeroMobile.jpg' for clarity
 import heroBgDesktop from '../../assets/images/Heroimage.svg';
 import heroBgMobile from '../../assets/images/HeroMobile.svg'; 
 
@@ -10,13 +8,11 @@ const Hero = () => {
   return (
     <section 
       className="hero-container" 
-      // 2. Pass both images as CSS Variables
       style={{ 
         '--bg-desktop': `url(${heroBgDesktop})`,
         '--bg-mobile': `url(${heroBgMobile})`
       }}
     >
-      {/* Overlay: Visible on Desktop, Hidden/Adjusted on Mobile */}
       <div className="hero-overlay"></div>
       
       <div className="hero-content">
